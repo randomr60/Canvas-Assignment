31af739e6efd7812981691aefaea8c168b87f662
root <root@Aaron>
2025-03-01 23:29:33 -0500

    Added info to file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-window.addEventListener("load", function(event) {
-    
+window.addEventListener("load", function (event) {
+    const canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth-100;
+        canvas.height = window.innerHeight-100;
+    }
+
+    window.addEventListener("resize", resizeCanvas);
+
+    resizeCanvas();
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 });
 
