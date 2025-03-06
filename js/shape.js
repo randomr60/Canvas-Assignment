26c90be3aa3db70863b20c36775f5141bf1b9346
root <root@Aaron>
2025-03-06 16:52:52 -0500

    Added a class called shape.js

---
+++ js/shape.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Shape {
+    
+    constructor(x_pos, y_pos, length, width) {
+        this.currentX = x_pos;
+        this.currentY = y_pos;
+        this.length = length;
+        this.width = width;
+    }
+    
+
+    
+    
+}
